c8a0e15d4826eeaf148fda97aab62bf56b8111c0
boxianglin <boxiang.lin@wsu.edu>
2023-06-23 14:50:50 -0700

    attempt to fix async load 404 error by a callback

--- dom/loader.js
+++ dom/loader.js
@@ -1,5 +1,15 @@
 const basicUILoad = () => {
-  $("#header").load("header.html");
+  $("#header").load("header.html", function () {
+    // load images in
+    $("#headImg").attr("src", "/img/book.svg");
+    $("#categoryImg").attr("src", "/img/category.svg");
+    $("#appImg").attr("src", "/img/app.svg");
+    console.log("Header loaded successfully.");
+  });
   $("#contentBlock").css("padding-top", "60px");
-  $("#footer").load("footer.html");
+  $("#footer").load("footer.html", function () {
+    // This is a callback that executes when the load is done.
+    console.log("Footer loaded successfully.");
+    // Any additional code interacting with elements in footer.html
+  });
 };
